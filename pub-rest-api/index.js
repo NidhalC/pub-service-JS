e110899980275fcf9b7948d8b99f96b81810deb3
Nidhal Chouaref <41987443+NidhalC@users.noreply.github.com>
2019-03-22 16:43:15 +0100

    passage es6+ des fichier js (#5)

--- pub-rest-api/index.js
+++ pub-rest-api/index.js
@@ -4,9 +4,9 @@ const pubService = require('pub-services').services.pubService;
 const app = express();
 app.get('/pubs', function (req, res) {
     const pubs = pubService.listPub();
-    res.status(200).json();
+    res.send(pubs);
 });
 
-var server = app.listen(3000, function() {
+const server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
